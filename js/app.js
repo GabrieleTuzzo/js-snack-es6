a7ecf33d831c97b99d82726fc46ecf24f698f46f
GabrieleTuzzo <gabriele.tuzzo@gmail.com>
2024-10-25 17:21:56 +0200

    CHANGE naming of functions and variables

--- js/app.js
+++ js/app.js
@@ -7,16 +7,16 @@ function getRandomIntInclusive(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min)
 }
 
-function populateSquadre() {
-    for (let i = 0; i < squadre.length; i++) {
-        const currentElement = squadre[i]
+function populateSoccerTeams() {
+    for (let i = 0; i < soccerTeams.length; i++) {
+        const currentElement = soccerTeams[i]
         currentElement['punti fatti'] = getRandomIntInclusive(0, 30)
         currentElement['falli subiti'] = getRandomIntInclusive(0, 50)
     }
 }
 
 // Snack #1
-const biciArray = [
+const bikesArray = [
     {
         nome: 'argon',
         peso: getRandomIntInclusive(minBikeWeight, maxBikeWeight),
@@ -31,18 +31,18 @@ const biciArray = [
     },
 ]
 
-console.log(biciArray)
+console.log(bikesArray)
 
-let lightestIndex
-let lightestWeight = biciArray[0].peso
-for (let i = 0; i < biciArray.length; i++) {
-    const currentWeight = biciArray[i].peso
+let lightestBikeIndex
+let lightestWeight = bikesArray[0].peso
+for (let i = 1; i < bikesArray.length; i++) {
+    const currentWeight = bikesArray[i].peso
     if (currentWeight <= lightestWeight) {
-        lightestIndex = i
+        lightestBikeIndex = i
     }
 }
 
-const lightestBike = biciArray[lightestIndex]
+const lightestBike = bikesArray[lightestBikeIndex]
 
 // Stampa solo L'ULTIMA bici più leggera trovata
 for (let key in lightestBike) {
@@ -50,7 +50,8 @@ for (let key in lightestBike) {
 }
 
 // Snack #2
-const squadre = [
+const printArray = []
+const soccerTeams = [
     {
         nome: 'Palermo',
         'punti fatti': 0,
@@ -83,11 +84,9 @@ const squadre = [
     },
 ]
 
-populateSquadre()
+populateSoccerTeams()
 
-const printArray = []
-
-squadre.forEach((element) => {
+soccerTeams.forEach((element) => {
     printArray.push(element.nome)
     printArray.push(element['falli subiti'])
 })
@@ -97,10 +96,10 @@ console.log(printArray)
 // Snack #3
 
 /**
- * Returns a new array by portioning a given array based on min index and maximum index, returns undefined if data is not correct
- * @param {*} array array to portion
- * @param {number} min the index from where to start the portion of the given array
- * @param {number} max the index from where to end the portion of the given array
+    Returns a new array by portioning a given array based on min index and maximum index, returns undefined if data is not correct
+    @param {*} array array to portion
+    @param {number} min the index from where to start the portion of the given array
+    @param {number} max the index from where to end the portion of the given array
  */
 function portionArray(array, min, max = array.length - 1) {
     const outputArray = []
@@ -120,4 +119,4 @@ function portionArray(array, min, max = array.length - 1) {
     return outputArray
 }
 
-console.log(portionArray(squadre, -2))
+console.log(portionArray(soccerTeams, -2))
